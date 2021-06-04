a572767ef8ea5138b2b98b5f54d9b4e7aa9fedc9
Victor Mukan <victor_mukan@epam.com>
2021-06-04 15:35:41 +0300

    Add e2e test for body validation

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,68 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { BulkService } from '../src/bulk.service';
 
-describe('AppController (e2e)', () => {
+describe('BulkController validation', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(BulkService)
+      .useValue({
+        sendRequest: () => {},
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/bulk 400 invalid url', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/bulk')
+      .send({
+        verb: 'GET',
+        url: 'a',
+        payload: [],
+      })
+      .expect(400);
+  });
+
+  it('/bulk 400 invalid verb', () => {
+    return request(app.getHttpServer())
+      .post('/bulk')
+      .send({
+        verb: 'g',
+        url: 'https://guesty-user-service.herokuapp.com/user/{userId}',
+        payload: [],
+      })
+      .expect(400);
+  });
+
+  it('/bulk 400 missing payload', () => {
+    return request(app.getHttpServer())
+      .post('/bulk')
+      .send({
+        verb: 'GET',
+        url: 'https://guesty-user-service.herokuapp.com/user/{userId}',
+      })
+      .expect(400);
+  });
+
+  it('/bulk 201', () => {
+    return request(app.getHttpServer())
+      .post('/bulk')
+      .send({
+        verb: 'GET',
+        url: 'https://guesty-user-service.herokuapp.com/user/{userId}',
+        payload: [],
+      })
+      .expect(201);
   });
 });
